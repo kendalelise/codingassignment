e706459eea38b4d2f1ba0a02d55c0eb1277eb74c
Xiang Chen <xinagchen@gmail.com>
2023-11-28 23:56:51 -0600

    worked on and updated the database

--- cart.py
+++ cart.py
@@ -1,3 +1,4 @@
+import sqlite3
 from userClass import User
 
 class Cart:
@@ -8,25 +9,53 @@ class Cart:
         self.cart_items = []
 
     def viewCart(self, userID, inventoryDatabase):
-        print(f"View cart: {userID}")
-        for item in self.cart_items:
-            print(f"ISBN: {item['ISBN']}")
+        print(f"View cart: {userID}\n")
+
+        conn = sqlite3.connect(self.databaseName)
+        cur = conn.cursor()
+        cur.execute(f"SELECT * FROM {self.tableName} INNER JOIN {inventoryDatabase} ON {self.tableName}.item_id = {inventoryDatabase}.item_id WHERE user_id = ?", (userID,))
+        cart_items = cur.fetchall()
+        conn.close()
+
+        return cart_items
+
 
     def addToCart(self, userID, ISBN):
         item = {"userID": userID, "ISBN": ISBN}
         self.cart_items.append(item)
+
+        conn = sqlite3.connect(self.databaseName)
+        cur = conn.cursor()
+        cur.execute("INSERT INTO Cart (user_id, ISBN) VALUES (?, ?)", (userID, ISBN))
+        conn.commit()
+        conn.close()
+
         print(f"Added item: {ISBN} to the cart")
 
     def removeFromCart(self, userID, ISBN):
-        for item in self.cart_items:
-            if item["userID"] == userID and item["ISBN"] == ISBN:
-                self.cart_items.remove(item)
-                print(f"Removed item: {ISBN} from the cart")
+        print(f"Item to remove from cart: {ISBN}")
+
+        conn = sqlite3.connect(self.databaseName)
+        cur = conn.cursor()
+        cur.execute("DELETE FROM Cart WHERE user_id = ? AND ISBN = ?", (userID, ISBN))
+        conn.commit()
+        conn.close()
+
+        print(f"item({ISBN}) removed.")
 
     def checkOut(self, userID, inventory):
         print(f"Checkout: {userID}")
-        for item in self.cart_items:
-            ISBN = item["ISBN"]
-            inventory.decreaseStock(ISBN)
-        self.cart_items = []
+
+        conn = sqlite3.connect(self.databaseName)
+        cur = conn.cursor()
+        cur.execute(f"SELECT ISBN, quantity FROM {self.tableName} WHERE user_id = ?", (userID,))
+        items = cur.fetchall()
+
+        for ISBN, quantity in items:
+            inventory.decreaseStock(ISBN, quantity)
+
+        cur.execute("DELETE FROM Cart WHERE user_id = ?", (userID,))
+        conn.commit()
+        conn.close()
+        
         print("Checkout completed.")
--- inventory.py
+++ inventory.py
@@ -1,13 +1,32 @@
+import sqlite3
 class Inventory:
     def __init__(self, databaseName="", tableName=""):
-        self.databaseName = databaseName
-        self.tableName = tableName
+        self.databaseName = "databaseTables.db"
+        self.tableName = "Inventory"
 
     def viewInventory(self):
-        print(f"Viewing inventory from database '{self.databaseName}' and table '{self.tableName}'")
+        print(f"Inventory View: \n\n")
+        conn = sqlite3.connect(self.databaseName)
+        cur = conn.cursor()
+        cur.execute(f"SELECT * FROM {self.tableName}")
+        inventory = cur.fetchall()
+        conn.close()
+        return inventory
 
-    def searchInventory(self):
-        print("Searching inventory")
+    def searchInventory(self, title):
+        print("Searching inventory...\n\n")
+        conn = sqlite3.connect(self.databaseName)
+        cur = conn.cursor()
+        cur.execute(f"SELECT * FROM {self.tableName} WHERE title LIKE ?", ('%' + title + '%',))
+        results = cur.fetchall()
+        conn.close()
+        return results
 
-    def decreaseStock(self, ISBN):
-        print(f"Decreasing stock for ISBN '{ISBN}'")
+    def decreaseStock(self, ISBN, quantity):
+        print(f"Decreasing stock for ISBN '{ISBN}': \n")
+
+        conn = sqlite3.connect(self.databaseName)
+        cur = conn.cursor()
+        cur.execute(f"UPDATE {self.tableName} SET stock = stock - ? WHERE ISBN = ?", (quantity, ISBN))
+        conn.commit()
+        conn.close()
